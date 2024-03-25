9b667a8e1c461d93bb9d56531f11c0d8555a5ebd
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-03-25 11:05:56 +0000

    auth & nav back to sign in

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -13,8 +13,8 @@ WebBrowser.maybeCompleteAuthSession();
 function SignInScreen() {
     const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
     const discovery = {
-        authorizationEndpoint: 'https://accounts.spotify.com/authorize',
-        tokenEndpoint: 'https://accounts.spotify.com/api/token',
+        authorizationEndpoint: 'http://localhost:3000/auth/spotify',
+        tokenEndpoint: 'http://localhost:3000/auth/spotify/callback',
     };
 
     const redirectUri = makeRedirectUri({
@@ -35,16 +35,18 @@ function SignInScreen() {
             // To follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
             // this must be set to false
             usePKCE: false,
-            redirectUri: "exp://127.0.0.1:19000/",
+            redirectUri: "exp://172.20.10.3:8081/",
         },
         discovery
     );
 
     useEffect(() => {
-        console.log("request ", request?.url);
+        console.log("request ", request);
+        WebBrowser.dismissBrowser();
+        WebBrowser.dismissAuthSession();
         console.log("RESPONSE BEFORE", response);
         if (response?.type === "success") {
-            const jwt = response.params.code;
+            const jwt = response.params.access_token;
             console.log("response IS ", response);
             storeAuthInfo(jwt);
 
@@ -62,6 +64,26 @@ function SignInScreen() {
             console.error("Error storing auth info", e);
         }
     };
+
+    //     const token = extractTokenFromUrl(result.url);
+    //     if (token) {
+    //         await AsyncStorage.setItem("@authToken", token);
+    //         navigation.navigate('HomeScreen'); // Navigate to the home screen
+    //     } else {
+    //         // Handle error or absence of token
+    //         console.error("Authentication failed");
+    //     }
+    // }
+    // };
+
+    // Function to extract the token from the redirect URL
+    const extractTokenFromUrl = (url: any) => {
+        // Implement based on how your backend sends the token
+        // Example: yourapp://redirect#token=YOUR_TOKEN
+        const match = url.match(/token=([^#]+)/);
+        return match ? match[1] : null;
+    };
+
 
     return (
         <LinearGradient colors={["#040306", "#131624"]} style={{ flex: 1 }}>
