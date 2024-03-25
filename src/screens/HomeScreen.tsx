ec19a7efa672a7b1daba34224bde8debb372f73b
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-03-25 21:47:34 +0000

    random size, color and duration bubbles falling

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,39 @@
-import { Button } from 'react-native';
-import { ParamListBase, useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import React, { useState, useEffect } from 'react';
+import { View, Dimensions } from 'react-native';
+import Circle from '../components/MusicBubble';
 
 
-export default function HomeScreen() {
-    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+const HomeScreen = () => {
+    const [circles, setCircles] = useState<any>([]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const newCircle = {
+                id: Math.random(),
+                size: Math.random() * 50,
+                color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+                duration: Math.random() * 2000 + 2000,
+                left: Math.random() * Dimensions.get('window').width,
+            };
+            setCircles((prevCircles: any) => [...prevCircles, newCircle]);
+        }, 500);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
-        <Button
-            title="Sign In with Spotify"
-            onPress={() => {
-                navigation.navigate('OnboardingScreen');
-
-                // Add your sign in with Spotify logic here
-            }}
-        />
+        <View style={{ flex: 1 }}>
+            {circles.map((circle: any) => (
+                <Circle
+                    key={circle.id}
+                    size={circle.size}
+                    color={circle.color}
+                    duration={circle.duration}
+                    left={circle.left}
+                />
+            ))}
+        </View>
     );
-}
+};
+
+export default HomeScreen;
