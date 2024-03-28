7a2b2eaca7ba89e48ed8b0dc32c3a92483b17298
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-03-28 15:43:07 +0000

    sign out

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,48 +1,66 @@
-import Animated, {
-    useSharedValue,
-    withTiming,
-    useAnimatedStyle,
-    Easing,
-} from 'react-native-reanimated';
-import { View, Button, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, Button, Alert, StyleSheet } from 'react-native';
+import { useAuthStore } from '../context/AuthContext';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
-export default function ProfileScreen() {
-    const randomWidth = useSharedValue(10);
+function ProfileScreen() {
+    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
 
-    const config = {
-        duration: 500,
-        easing: Easing.bezier(0.5, 0.01, 0, 1),
+    const { signOut } = useAuthStore();
+    const handleSignOut = () => {
+        Alert.alert(
+            'Confirm ',
+            'Are you sure you want to sign out?',
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Confirm',
+                    onPress: () => {
+                        // Logic to sign out the user
+                        // This can include clearing user session, redirecting, etc.
+                        signOut();
+                        navigation.reset({
+                            index: 0,
+                            routes: [{ name: 'SignInScreen' }],
+                        });
+                    },
+                },
+            ],
+            { cancelable: false }
+        );
     };
-
-    const style = useAnimatedStyle(() => {
-        return {
-            width: withTiming(randomWidth.value, config),
-        };
-    });
 
     return (
         <View style={styles.container}>
-            <Animated.View style={[styles.box, style]} />
-            <Button
-                title="toggle"
-                onPress={() => {
-                    randomWidth.value = Math.random() * 350;
-                }}
-            />
+            <Text style={styles.text}>Profile Page</Text>
+            <Button title="Sign Out" onPress={handleSignOut} />
         </View>
     );
 }
 
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: 'yellow'
     },
-    box: {
-        width: 100,
-        height: 80,
-        backgroundColor: 'black',
-        margin: 30,
+    text: {
+        fontSize: 20,
+        color: 'white',
+        marginBottom: 20
     },
+    button: {
+        backgroundColor: 'red',
+        padding: 10,
+        borderRadius: 5
+    }
 });
+
+export default ProfileScreen;
