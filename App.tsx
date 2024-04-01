a2f1348f03be23a4f560a8cdabcd6dabaae2e41a
Yaw Botwe <ybotwe@gmail.com>
2024-04-01 21:14:45 +0000

    onboarding and sign in done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import OnboardingScreen from './src/screens/OnboardingScreen';
-import SignInScreen from './src/screens/SignInScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import 'react-native-gesture-handler';
-import LandingScreen from './src/screens/LandingScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import OnboardingScreen from "./src/screens/OnboardingScreen";
+import SignInScreen from "./src/screens/SignInScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import "react-native-gesture-handler";
+import LandingScreen from "./src/screens/LandingScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import * as Sentry from "@sentry/react-native";
-import TutorialScreen from './src/screens/TutorialScreen';
-import { useEffect, useState } from 'react';
-import { useAuthStore } from './src/context/AuthContext';
-import ProfileScreen from './src/screens/ProfileScreen';
-
+import TutorialScreen from "./src/screens/TutorialScreen";
+import { useEffect, useState } from "react";
+import { useAuthStore } from "./src/context/AuthContext";
+import ProfileScreen from "./src/screens/ProfileScreen";
 
 Sentry.init({
   dsn: "https://cd7bf1148cfa1fe20a662190408cab95@o4506932229636096.ingest.us.sentry.io/4506932245168128",
@@ -23,71 +23,84 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
-
-
 const Stack = createStackNavigator();
 
 function App() {
-  const istest = false; // Add your logic to check if the user is signed in
-  const { isSignedIn, signIn, signOut } = useAuthStore();
-  const initialRouteName = isSignedIn ? 'HomeScreen' : 'SignInScreen';
 
+  const { isSignedIn, signIn, signOut } = useAuthStore();
+  const [isFirstLaunch, setIsFirstLaunch] = useState<Boolean>(false);
+
+  useEffect(() => {
+    checkIsFirstLaunch();
+  }, []);
+
+  
+  
+  const checkIsFirstLaunch = async () => {
+    const value = await AsyncStorage.getItem("@alreadyLaunched");
+    if (value === null) {
+      // If "@alreadyLaunched" is not set, it's the first launch
+      AsyncStorage.setItem("@alreadyLaunched", "true"); // Mark as launched
+      setIsFirstLaunch(true);
+    } else {
+      setIsFirstLaunch(false); // Here we make sure the state is updated
+    }
+  };
+
+  // Determine the initial route based on the states
+  let initialRoute;
+  if (isFirstLaunch === true) {
+    initialRoute = "OnboardingScreen";
+  } else {
+    initialRoute = isSignedIn ? "HomeScreen" : "SignInScreen";
+  }
 
   return (
-
     // <AuthProvider>
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={initialRouteName}>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName={initialRoute}
+      >
         <Stack.Screen
           name="HomeScreen"
           component={HomeScreen}
-          options={{ title: 'Home' }}
+          options={{ title: "Home" }}
         />
         <Stack.Screen
           name="OnboardingScreen"
           component={OnboardingScreen}
           options={{
-            title: 'Onboarding',
+            title: "Onboarding",
             headerShown: false,
           }}
         />
         <Stack.Screen
           name="SignInScreen"
           component={SignInScreen}
-          options={{ title: 'Sign In' }}
+          options={{ title: "Sign In" }}
         />
         <Stack.Screen
           name="TutorialScreen"
           component={TutorialScreen}
-          options={{ title: 'Tutorial' }}
+          options={{ title: "Tutorial" }}
         />
         <Stack.Screen
           name="ProfileScreen"
           component={ProfileScreen}
-          options={{ title: 'Profile' }}
+          options={{ title: "Profile" }}
         />
-        {Platform.OS === 'web' && (
+        {Platform.OS === "web" && (
           <Stack.Screen
             name="LandingScreen"
             component={LandingScreen}
-            options={{ title: 'Landing' }}
+            options={{ title: "Landing" }}
           />
         )}
       </Stack.Navigator>
     </NavigationContainer>
     // </AuthProvider>
-
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
 
 export default Sentry.wrap(App);
--- src/screens/OnboardingScreen.tsx
+++ src/screens/OnboardingScreen.tsx
@@ -1,45 +1,73 @@
-import React from 'react';
-import { Image, StyleSheet } from 'react-native';
-import Onboarding from 'react-native-onboarding-swiper';
-import { ParamListBase, useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import React from "react";
+import Onboarding from "react-native-onboarding-swiper";
+import LottieView from "lottie-react-native";
+import { ParamListBase, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
-export default function OnboardingScreen() {
+const OnboardingScreen = () => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
 
+  const onDone = () => {
+    AsyncStorage.setItem("@alreadyLaunched", "true");
+    navigation.navigate("SignInScreen");
+  };
+
+  const onSkip = () => {
+    AsyncStorage.setItem("@alreadyLaunched", "true");
+    navigation.replace("SignInScreen"); // Use replace to avoid going back to onboarding
+  };
 
   return (
     <Onboarding
-
-      onDone={() => navigation.navigate('SignInScreen')}
-      onSkip={() => navigation.navigate('SignInScreen')}
+      onSkip={onSkip}
+      onDone={onDone}
       pages={[
         {
-          backgroundColor: '#fff',
-          image: <Image source={require('../../assets/airplanemode.jpg')} style={{ width: 100, height: 100 }} />,
-          title: 'Welcome to Our App',
-          subtitle: 'This is a cool music app',
+          backgroundColor: "#131624",
+          image: (
+            <LottieView
+              source={require("../../assets/animation1.json")}
+              autoPlay
+              loop
+              style={{ width: 300, height: 300 }}
+            />
+          ),
+          title: "Discover in Every Tap",
+          subtitle:
+            "Welcome to a world where every bubble brings you closer to your next favorite track.",
         },
         {
-          backgroundColor: '#fff',
-          image: <Image source={require('../../assets/airplanemode.jpg')} style={{ width: 100, height: 100 }} />,
-          title: 'Discover New Music',
-          subtitle: 'Find your next favorite song',
+          backgroundColor: "#333399",
+          image: (
+            <LottieView
+              source={require("../../assets/animation2.json")}
+              autoPlay
+              loop
+              style={{ width: 300, height: 300 }}
+            />
+          ),
+          title: "Interactive Listening",
+          subtitle:
+            "Interact with music like never before. Press the bubbles to play sounds and explore the melodies that move you.",
         },
         {
-          backgroundColor: '#fff',
-          image: <Image source={require('../../assets/airplanemode.jpg')} style={{ width: 100, height: 100 }} />,
-          title: 'Build Your Library',
-          subtitle: 'Swipe up to add songs to your library',
-          titleStyles: {
-            fontSize: 20,
-            color: '#E01C1C',
-            alignContent: "flex-start"
-          }
+          backgroundColor: "#444655",
+          image: (
+            <LottieView
+              source={require("../../assets/animation3.json")}
+              autoPlay
+              loop
+              style={{ width: 300, height: 300 }}
+            />
+          ),
+          title: "Swipe to Save",
+          subtitle:
+            "Found a beat that caught your ear? Swipe up on the bubble to add it to your library and keep the vibes flowing.",
         },
       ]}
     />
   );
-}
+};
 
+export default OnboardingScreen;
